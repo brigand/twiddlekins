af2559561753e906c8ee1162efd8789f5784f5a7
Frankie Bagnardi <f.bagnardi@gmail.com>
2011-09-28 17:16:21 -0400

    replaced goo.gl with bit.ly for shortening

--- Utility/HTTP.cs
+++ Utility/HTTP.cs
@@ -2,13 +2,15 @@
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Web;
 
 namespace robokins.Utility
 {
     class HTTP
     {
         const string UserAgent = "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100722 Ubuntu/10.04 (lucid) Firefox/3.6.8";
-
+		public static bool useahk4me = true;	
+		
         public static string DownloadPage(string uri)
         {
             try
@@ -60,17 +62,24 @@ namespace robokins.Utility
 
         public static string ShortUrl(string url)
         {
-            const string service = "https://www.googleapis.com/urlshortener/v1/url";
-            var request = string.Concat("{\"longUrl\":\"", Uri.EscapeUriString(url), "\"}");
-
-            var result = DownloadPage(service, request, null, "application/json");
+            const string service = "http://api.bitly.com/v3/shorten?login={0}&apiKey={1}&longUrl={2}&format=txt";		
+			
+			// This key is Frankie's personal key.  They may stop working at any time.
+			// Consider replacing them after testing.
+			string api_login = "o_26thqndlmc";
+			string api_key = "R_954a40faa0cdcdcb5c0b48af321f6830";
+			
+			if (useahk4me) {
+				api_login = "ahk4me";
+				api_key = "R_4b3df1f5417d94ff356ed511fd50a153";
+			}
+            var request = string.Format(service, api_login, api_key, HttpUtility.UrlEncode(url));
+            var result = DownloadPage(request);
 
             if (string.IsNullOrEmpty(result))
                 return url;
-
-            const string domain = "http://goo.gl/";
-            var id = Texts.StringBetween(result, domain, "\"");
-            return domain + id;
+			
+            return result;
         }
     }
 }
