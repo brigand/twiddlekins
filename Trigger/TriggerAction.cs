c0bde4b13611c86216a1d21051240b1177ba2a47
Frankie Bagnardi <f.bagnardi@gmail.com>
2011-10-07 21:29:39 -0400

    Added and tested Handle(), ActionFromWrapper().  Few minor fixes

--- Trigger/TriggerAction.cs
+++ Trigger/TriggerAction.cs
@@ -11,7 +11,7 @@ namespace robokins.Trigger
 	/// <returns>
 	/// True to signal the message is ready to be sent.
 	/// </returns>
-	delegate bool TriggerHandleDelegate(ReceivedMessage msg, TriggerResponse resp);
+	delegate bool TriggerHandleDelegate(TriggerAction act, ReceivedMessage msg, ref TriggerResponse resp);
 	
 	partial class TriggerAction
 	{
@@ -21,7 +21,7 @@ namespace robokins.Trigger
 		protected string startsWith = string.Empty;
 		protected string endsWith = string.Empty;
 		protected bool encode;
-		protected string returnformat = string.Empty;
+		protected string returnFormat = string.Empty;
 		protected int requiredArguments;
 		protected TriggerHandleDelegate action;
 		#endregion
@@ -72,8 +72,9 @@ namespace robokins.Trigger
 
 		public string ReturnFormat
 		{
-			get { return returnformat; }
+			get { return returnFormat; }
 		}
+		
 
 		public int RequiredArguments
 		{
@@ -81,30 +82,44 @@ namespace robokins.Trigger
 		}
 		#endregion
 
-		/// <summary>
-		/// Creates a new trigger action using the provided information.
-		/// </summary>
-		/// <param name="search">Search URI to use when performing the search</param>
-		/// <param name="match">String used for "simple" matching against returned data in a search</param>
-		/// <param name="starts">Start string used when extracting sections from returned data</param>
-		/// <param name="ends">End string used when extracting sections from returned data</param>
-		/// <param name="enc">Tells the handler if we want the user input encoded or not</param>
-		public TriggerAction(string search, 
-		                     string match, string starts, string ends, 
-		                     bool enc, string _returnformat, int requiredarguments)
-		{
-			searchUri = search;
-			matchString = match;
-			startsWith = starts;
-			endsWith = ends;
-			encode = enc;
-			returnformat = _returnformat;
-			requiredArguments = requiredarguments;
-			
-			
-		}
+		
+		
 		#region Overloads
 		
+		/// <summary>
+		/// Construct a new TriggerAction that searches a web-page using the
+		/// the normal replacement format for TriggerActions.  
+		/// </summary>
+		/// <param name="uri">
+		/// The uri to be downloaded and parsed. 
+		/// 
+		/// All C# rules for formatted string apply.  {0} matches the first word 
+		/// after the trigger, {1} the second, and so on.  {all} and {last} match
+		/// the entire search string.  All input is url safe encoded.
+		/// </param>
+		/// <param name="starts_with">
+		/// The starting text for a match.  This text isn't included in output.
+		/// </param>
+		/// <param name="ends_with">
+		/// The ending text for a match.
+		/// </param>
+		/// <param name="format">
+		/// Specifies the response format.  If blank or null it defaults to the 
+		/// matched text.  It uses the <see cref="System.String.Format"> function
+		/// where {0} will contain the entire match.
+		/// </param>
+		/// <returns>
+		/// A <see cref="TriggerAction"/>
+		/// </returns>
+		public static TriggerAction ActionFromWrapper(string uri, string starts_with, string ends_with, string format)
+		{
+			TriggerAction ta = new TriggerAction(HandlePageSearch);
+			ta.searchUri = uri;
+			ta.startsWith = starts_with;
+			ta.endsWith = ends_with;
+			ta.returnFormat = format;
+			return ta;
+		}
 		
 		public TriggerAction(TriggerHandleDelegate action)
 		{
@@ -113,6 +128,14 @@ namespace robokins.Trigger
 
 
 		#endregion
-
+		
+		public bool Handle(ReceivedMessage msg, ref TriggerResponse resp)
+		{
+			// A round-about way of doing this, but it works
+			// There should be a simpler way to give the delegate access
+			// to 'this'
+			return this.action(this, msg, ref resp);
+		}
+		
 	}
 }
